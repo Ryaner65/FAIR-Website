6fec1242569acffd8c3793717f22f35d417c32a1
patrickweiss <patrick.weiss@officeone.team>
2022-09-07 10:29:36 +0200

    webapp

--- js/mobileApp.js
+++ js/mobileApp.js
@@ -1,12 +1,50 @@
-console.log("Version 0020")
+console.log("Version 0026")
 
-const responsiveMobile = "responsiveMobile";
-const digitaleAktivierung="digitaleAktivierung";
-const digitaleOrientierung="digitaleOrientierung";
-const digitaleGrundlagen="digitaleGrundlagen";
-const DevOps="DevOps";
-const exiCoaching="exiCoaching";
-const bewerberCoaching="bewerberCoaching";
+
+//Inhalte for produkt "responsiveMobile" merken
+let homeInhalt = [];
+
+//Reihenfolge für Laptop und Mobile definieren
+const laptop = [0, 1, 2, 3, 4, 5]
+const mobile = [0, 1, 3, 2, 4, 5]
+
+//Nach dem laden wird die Seite an das produkt angepasst
+window.addEventListener('DOMContentLoaded', event => {
+    neueSeite();
+    try {
+        homeInhalt = [
+            document.getElementById("laptop_0").innerHTML,
+            document.getElementById("laptop_1").innerHTML,
+            document.getElementById("laptop_2").innerHTML,
+            document.getElementById("laptop_3").innerHTML,
+            document.getElementById("laptop_4").innerHTML,
+            document.getElementById("laptop_5").innerHTML
+        ]
+        inhaltResponsive();
+        window.onresize = inhaltResponsive;
+    } catch (error) {
+        console.log(error);
+    }
+    return
+});
+
+const originalHTML = document.getElementById("page-top").innerHTML;
+
+
+function inhaltResponsive() {
+    if (window.innerWidth > 1000) {
+        for (let i = 0; i < homeInhalt.length; i++) {
+            document.getElementById("laptop_" + i).innerHTML = homeInhalt[laptop[i]];
+            console.log("jetzt laptop")
+        }
+    } else {
+        for (let i = 0; i < homeInhalt.length; i++) {
+            document.getElementById("laptop_" + i).innerHTML = homeInhalt[mobile[i]];
+            console.log("jetzt mobile")
+        }
+    }
+}
+
 
 //get paramater from URL and set the corresponding page
 function getParameterByName(name, url) {
@@ -20,75 +58,28 @@ function getParameterByName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
-let produkt=getParameterByName("produkt");
-if (!produkt)produkt=responsiveMobile;
-console.log(produkt);
+let player_code = getParameterByName("player_code");
+if (!player_code) player_code = "unbekannt";
+console.log(player_code);
 
 
-//Inhalte for produkt "responsiveMobile" merken
-const homeInhalt = [
-    document.getElementById("laptop_0").innerHTML,
-    document.getElementById("laptop_1").innerHTML,
-    document.getElementById("laptop_2").innerHTML,
-    document.getElementById("laptop_3").innerHTML,
-    document.getElementById("laptop_4").innerHTML,
-    document.getElementById("laptop_5").innerHTML
-]
-//Reihenfolge für Laptop und Mobile definieren
-const laptop = [0, 1, 2, 3, 4, 5]
-const mobile = [0, 1, 3, 2, 4, 5]
+//call REST API at https://script.google.com/macros/s/AKfycbxbCJLyP8PYbH4ty4TWtF-q0W6F1JSqI8wR2TTb_kOvuoTyNqQNkqifeHhDJi3ZTXKF/exec
+let url = "https://script.google.com/macros/s/AKfycbxbCJLyP8PYbH4ty4TWtF-q0W6F1JSqI8wR2TTb_kOvuoTyNqQNkqifeHhDJi3ZTXKF/exec";
 
-//Nach dem laden wird die Seite an das produkt angepasst
-window.addEventListener('DOMContentLoaded', event => {
-    if (produkt===responsiveMobile){
-        inhaltResponsive();
-        window.onresize = inhaltResponsive;
-        return
-    }
-    inhaltProdukt(produkt);
-});
-
-const produktHTML=`
-<iframe src="https://script.google.com/a/macros/saw-office.net/s/AKfycbw_faHfieWbT5DT27mqHUdBWWfWmvq-DT5sD-0UpnAZJ-X9FrpggoqAFVaHgSa2E1LG1g/exec" title="bestellen" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">
-    Dein Browser unterstützt diese Anwendung nicht.
-</iframe>
-`
-const originalHTML = document.getElementById("page-top").innerHTML;
-
-function inhaltProdukt(produkt) {
-    console.log("fair-sz redirect zu produkt: "+produkt);
-    if (produkt===digitaleAktivierung) document.getElementById("page-top").innerHTML = produktHTML;
-
-    //Redirect to https://sites.google.com/fair-sz.com/fairschulungszentrum/bildungsangebote/digitale-orientierung
-    if (produkt===digitaleOrientierung) window.location.href = "https://sites.google.com/fair-sz.com/fairschulungszentrum/bildungsangebote/digitale-orientierung";
-
-    //Redirect to https://sites.google.com/fair-sz.com/fairschulungszentrum/bildungsangebote/digitale-grundlagen
-    if (produkt===digitaleGrundlagen) window.location.href = "https://sites.google.com/fair-sz.com/fairschulungszentrum/bildungsangebote/digitale-grundlagen"; 
-
-    //Redirect to https://sites.google.com/fair-sz.com/fairschulungszentrum/bildungsangebote/softwareentwicklung-und-scrum-master
-    if (produkt===DevOps) window.location.href = "https://sites.google.com/fair-sz.com/fairschulungszentrum/bildungsangebote/softwareentwicklung-und-scrum-master";
-
-    //Redirect to https://sites.google.com/fair-sz.com/fairschulungszentrum/bildungsangebote/softwareentwicklung-und-scrum-master
-    if (produkt===exiCoaching) window.location.href = "https://sites.google.com/fair-sz.com/fairschulungszentrum/bildungsangebote/existenzgr%C3%BCnder-coaching";
-
-    //Redirect to https://sites.google.com/fair-sz.com/fairschulungszentrum/bildungsangebote/softwareentwicklung-und-scrum-master
-    if (produkt===bewerberCoaching) window.location.href = "https://sites.google.com/fair-sz.com/fairschulungszentrum/bildungsangebote/bewerbercoaching";
-}
-
-
-
-
-function inhaltResponsive() {
-    if (window.innerWidth > 1000) {
-        for (let i = 0; i < homeInhalt.length; i++) {
-            document.getElementById("laptop_"+i).innerHTML = homeInhalt[laptop[i]];
-            console.log("jetzt laptop")
+function neueSeite() {
+    console.log("neueSeite");
+    fetch(url + "?produkt=" + window.location.href + "&player_code=" + player_code, {
+        redirect: "follow",
+        method: "GET",
+        headers: {
+            "Content-Type": "text/plain;charset=utf-8"
         }
-    } else {
-        for (let i = 0; i < homeInhalt.length; i++) {
-            document.getElementById("laptop_"+i).innerHTML = homeInhalt[mobile[i]];
-            console.log("jetzt mobile")
-        }
-    }
+    }).then(function (response) {
+        console.log(response);
+        return response.json();
+    }).then(function (data) {
+        console.log("Text" + JSON.stringify(data));
+        //set the page according to the produkt
+        //    window.location.href = data.url
+    })
 }
-
--- massnahmen.php
+++ massnahmen.php
@@ -6,7 +6,7 @@ include("include/header.php");
             <div class="container">
 				<h2>FAIR GmbH - Der Verantwortung verpflichtet</h2>
 <p>Immer mehr Menschen sind auf der Suche nach Antworten zur beruflichen Neuorientierung. Hier setzt unsere Daseinsberechtigung an. Als nach AZAV zertifizierter Bildungstr&auml;ger gew&auml;hrleisten wir durch die Gestaltung von Kursen, Ma&szlig;nahmen und Coachings eine qualifizierte Weiterentwicklung f&uuml;r die berufliche Weiterbildung aller unserer Teilnehmer. Lernen Sie uns pers&ouml;nlich kennen und vereinbaren Sie einen kostenfreien Termin, telefonisch oder per Video-Gespr&auml;ch.</p>
-<h2 >f&uuml;r Gr&uuml;nder: Erfolgreiche Gr&uuml;ndungsberatung</h2>
+<h2 >F&uuml;r Gr&uuml;nder: Erfolgreiche Gr&uuml;ndungsberatung</h2>
 <p >In unserem Existenzgr&uuml;ndercoaching nehmen wir Sie vor der Gr&uuml;ndung an die Hand und vermitteln alle rationalen und emotionalen Aspekte einer Gr&uuml;ndung. Ziel der Beratung ist es, dass Sie entscheiden k&ouml;nnen, ob der Weg in die Selbstst&auml;ndigkeit f&uuml;r Sie der richtige ist.</p>
 <p><span >Was Sie von der Beratung erwarten d&uuml;rfen, wie die Beratung stattfindet und wie Gr&uuml;ndungsberatung gef&ouml;rdert wird erfahren Sie <a href="gruender.php">hier</a>.</span></p>
 <h2 >F&uuml;r Jungunternehmer und Unternehmer: Unternehmensberatung</h2>
