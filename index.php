b081812a57413c40857b7bb2cc195e5c74e4b2f4
beigelit <sbeigel@beigel-it.de>
2022-11-22 20:23:36 +0100

    Home
    Angebote auf Startseite geaddet,
    Broken Links removed

--- index.php
+++ index.php
@@ -72,6 +72,102 @@ mit Ihm auszuprobieren, Ihm Klarheit zu bringen</p>
 				</div>                
             </div>
         </section>
+        <section class="page-section" >
+            <div class="container">  
+                <div class="row">              
+                <h1>Angebote</h1>
+                    <div class="col-md-4">
+                        <div class="card" style="width: 18rem;; margin: 2%;">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="card-link">Card link</a>
+                                <a href="#" class="card-link">Another link</a>
+                            </div>
+                        </div> 
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card" style="width: 18rem;; margin: 2%;">
+                            <div class="card-body">
+                                <h5 class="card-title">Digitale Orientierung</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="card-link">Card link</a>
+                                <a href="#" class="card-link">Another link</a>
+                            </div>
+                        </div> 
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card" style="width: 18rem;; margin: 2%;">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="card-link">Card link</a>
+                                <a href="#" class="card-link">Another link</a>
+                            </div>
+                        </div> 
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="card" style="width: 18rem;; margin: 2%;">
+                            <div class="card-body">
+                                <h5 class="card-title">digitale Grundlagen</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="card-link">Card link</a>
+                                <a href="#" class="card-link">Another link</a>
+                            </div>
+                        </div> 
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card" style="width: 18rem;; margin: 2%;">
+                            <div class="card-body">
+                                <h5 class="card-title">Softwareentwicklung</h5>
+                                <h6 class="card-subtitle mb-2 text-muted"> & SCRUM Master</h6>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="card-link">Card link</a>
+                                <a href="#" class="card-link">Another link</a>
+                            </div>
+                        </div> 
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card" style="width: 18rem;; margin: 2%;">
+                            <div class="card-body">
+                                <h5 class="card-title">Existenzgründer Coaching</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="card-link">Card link</a>
+                                <a href="#" class="card-link">Another link</a>
+                            </div>
+                        </div> 
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card" style="width: 18rem;; margin: 2%;">
+                            <div class="card-body">
+                                <h5 class="card-title">Bewerbercoaching</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="card-link">Card link</a>
+                                <a href="#" class="card-link">Another link</a>
+                            </div>
+                        </div> 
+                    </div>
+                    <div class="col-md-4">
+                        <div class="card" style="width: 18rem;; margin: 2%;">
+                            <div class="card-body">
+                                <h5 class="card-title">in Entwicklung befindlich</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="card-link">Card link</a>
+                                <a href="#" class="card-link">Another link</a>
+                            </div>
+                        </div> 
+                    </div>
+            </div>             
+            </div>
+        </section>
 
 
         <?php
